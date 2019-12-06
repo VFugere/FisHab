4b833286e643a629dc95a78fca979f13c3f26ec0
V_Fugere <vincent.fugere@mail.mcgill.ca>
2019-12-05 22:58:33 -0500

    added a couple of init files + MFFP script

---
+++ MFFP_explore.R
@@ -0,0 +1,189 @@
+# code to add RHS numbers to all lake pulse sites and all Qc fish datasets
+
+rm(list=ls())
+
+library(tidyverse)
+library(readxl)
+library(scales)
+
+# 0) load GIS info with LCE and RHS databases
+
+LCE <- read_xlsx('/Users/vincentfugere/Google Drive/Recherche/Lake Pulse Postdoc/GIS/tous_les_LCE.xlsx')
+
+# RHS <- readOGR(dsn = "/Users/vincentfugere/Google Drive/Recherche/Lake Pulse Postdoc/GIS/RHS/", layer = "RHS")
+# proj4string(RHS)
+
+# 1) add RHS to all Lake Pulse sites
+
+LP_LCE <- read_xlsx('/Users/vincentfugere/Google Drive/Recherche/Lake Pulse Postdoc/GIS/LP_Qc_NoLacLCE.xlsx') %>%
+  rename(RHS = ID_RHS)
+
+PS <- read_xlsx(skip=2,'/Users/vincentfugere/Google Drive/Recherche/Lake Pulse Postdoc/data/MFFP/IFA - Rapport Pêche Sportive 2000-2018.xlsx')
+colnames(PS)[2] <- 'LCE'
+n_distinct(PS$LCE) #9135 sites!
+PS %>% distinct(LCE) %>% select(LCE) -> ps_sites
+ps_sites <- left_join(ps_sites, LCE)
+table(ps_sites$ecosysteme) #8918 lacs, 53 rivières
+
+INV <- read_xlsx(skip=5,"/Users/vincentfugere/Google Drive/Recherche/Lake Pulse Postdoc/data/MFFP/IFD- 1 - Rapport Inventaire sur plan d'eau (IPE) 2000-2018.xlsx")
+colnames(INV)[1] <- 'LCE'
+n_distinct(INV$LCE) #3260 sites!
+INV %>% distinct(LCE) %>% select(LCE) -> inv_sites
+inv_sites <- left_join(inv_sites, LCE)
+table(inv_sites$ecosysteme) #2357 lacs, 864 rivières
+table(INV$`Type de pêche`)
+
+# 3) combien se recoupent?
+
+sum(LP_LCE$LCE %in% INV$LCE)
+sum(LP_LCE$LCE %in% PS$LCE)
+sum(LP_LCE$LCE %in% c(PS$LCE,INV$LCE))
+
+inv_lp <- filter(INV, LCE %in% LP_LCE$LCE)
+table(inv_lp$`Type de pêche`)
+#surtout de la pêche non-normalisée..
+
+### GRADIENTS taille-latitude ####
+
+inv <- INV
+rm(INV)
+
+inv$max.l <- as.numeric(inv$`Long max (mm)`)
+
+inv <- inv %>% group_by(LCE, Espèce) %>%
+  summarize(lat = mean(Latitude, na.rm=T), long = mean(Longitude, na.rm=T), size = max(max.l, na.rm=T))
+colnames(inv)[2] <- 'species'
+
+inv <- inv %>% filter(!is.na(lat))
+inv <- inv %>% filter(!is.na(size)) %>% filter(is.finite(size))
+inv <- as.data.frame(inv)
+
+data <- inv
+
+data <- data %>% add_count(species)
+
+taxlist <- data %>% distinct(species) %>% pull(species)
+data$scl.size <- 0
+data$scl.lat <- 0
+for(i in 1:length(taxlist)){
+  tmp <- filter(data, species == taxlist[i])
+  scl.size <- scale(tmp$size, scale = T)[,1]
+  scl.lat <- scale(tmp$lat, scale = F)[,1]
+  data[data$species == taxlist[i], 'scl.size'] <- scl.size
+  data[data$species == taxlist[i], 'scl.lat'] <- scl.lat
+}
+
+data$species <- as.factor(data$species)
+data$scl.size[!is.finite(data$scl.size)] <- NA
+data <- data %>% filter(size > 0)
+
+reg.results <- data.frame('focalsp' = character(0),
+                          'min.lat' = numeric(0),
+                          'max.lat' = numeric(0),
+                          'n' = numeric(0),
+                          'slope' = numeric(0),
+                          'SE' = numeric(0),
+                          'p' = numeric(0),
+                          'r2' = numeric(0), 
+                          'mean.size' =numeric(0),
+                          stringsAsFactors = F)
+
+for(i in 1:nlevels(data$species)){
+  focalsp <- levels(data$species)[i]
+  spdat <- data %>% filter(species == focalsp)
+  spdat <- spdat %>% drop_na(scl.size)
+  if(nrow(spdat) >= 10){
+    #lmmod <- lm(ind.size~x,spdat)
+    lmmod <- lm(scl.size~lat,spdat)
+    r2 <- summary(lmmod)$r.squared
+    slope <- coef(lmmod)[2]
+    p <- summary(lmmod)$coefficients[2,4]
+    SE <- summary(lmmod)$coefficients[2,2]
+    n <- nrow(spdat)
+    min.lat <- min(spdat$lat)
+    max.lat <- max(spdat$lat)
+    mean.size <- mean(spdat$size)
+    results1 <- c(focalsp)
+    results2 <- c(min.lat,max.lat,n,slope,SE,p,r2,mean.size)
+    reg.results[nrow(reg.results)+1,1] <- results1
+    reg.results[nrow(reg.results),2:9] <- results2
+  }
+}
+
+reg.results$xrange <- reg.results$max.lat-reg.results$min.lat
+
+reg.results$bubblesize <- rescale(reg.results$r2, c(1,4))
+reg.results$bubblecol <- 'dark gray'
+reg.results$bubblecol[reg.results$p < 0.05 & reg.results$slope < 0] <- 'red'
+reg.results$bubblecol[reg.results$p < 0.05 & reg.results$slope > 0] <- 'blue'
+reg.results$bubble.pch <- 16
+reg.results$bubble.pch[reg.results$bubblecol == 'dark gray'] <- 1
+
+hist(reg.results$slope, breaks=50)
+# 
+#plot à la Dornelas
+plot(r2~slope,reg.results,bty='n',pch=16,col=alpha(bubblecol,0.5),cex=bubblesize)
+abline(v=0,lty=2)
+#not very exciting...
+
+#####
+
+pdf('~/Desktop/regressions.pdf',width=8,height=7,pointsize=14)
+
+plotfunctions::emptyPlot(xlim = range(data$lat),yaxt='n',xaxt='n',ann=F, ylim=c(0,quantile(data$size,0.99)),bty='l')
+axis(2,cex.axis=1,lwd=0,lwd.ticks=1)
+axis(1,cex.axis=1,lwd=0,lwd.ticks=1)
+title(xlab='latitude')
+title(ylab='max length (mm)',line=2.8)
+for(i in 1:nlevels(data$species)){
+  focalsp <- levels(data$species)[i]
+  spdat <- data %>% filter(species == focalsp) %>% arrange(lat)
+  spdat <- filter(spdat, !is.na(scl.size))
+  if(nrow(spdat) >= 10){
+    lmmod <- lm(scl.size~lat,spdat)
+    lncol <- 'dark gray'
+    lnwd <- 1
+    if(summary(lmmod)$coefficients[2,4] < 0.05){
+      lnwd <- 1
+      if(summary(lmmod)$coefficients[2,1] > 0){lncol <- 'blue'}else{
+        lncol <- 'red'}
+    }
+    lmmod <- lm(size~lat,spdat)
+    points(fitted(lmmod)~spdat$lat,col=alpha(lncol,0.5),type='l',lwd=lnwd)
+  }
+}
+
+dev.off()
+
+##### slopes  #####
+
+pdf('~/Desktop/slopes.pdf',width=14,height=6,pointsize=14)
+par(mfrow=c(1,3),cex=1,mar=c(4,2.5,0,0),oma=c(0,2,0,0))
+
+plot(slope~n,reg.results,bty='n',pch=bubble.pch,col=alpha(bubblecol,0.5),cex=bubblesize,bty='l',yaxt='n',xaxt='n',ann=F, log = 'x')
+axis(2,cex.axis=1,lwd=0,lwd.ticks=1)
+axis(1,cex.axis=1,lwd=0,lwd.ticks=1)
+title(xlab=number~of~lakes,line=2.8)
+abline(h=0,lty=3)
+
+mtext(expression(Delta~body~mass~per~degree~latitude~(sd)),outer = T,side=2,line=0.5)
+
+# which.max(reg.results$n)
+# reg.results[244,]
+# reg.results %>% arrange(n) %>% pull(focalsp)
+
+plot(slope~xrange,reg.results,bty='n',pch=bubble.pch,col=alpha(bubblecol,0.5),cex=bubblesize,bty='l',yaxt='n',xaxt='n',ann=F,xlim=(range(reg.results$xrange) + c(-0.5,0.5)))
+axis(2,cex.axis=1,lwd=0,lwd.ticks=1)
+axis(1,cex.axis=1,lwd=0,lwd.ticks=1)
+title(xlab='latitudinal range',line=2.8)
+#title(ylab=slope~(beta),line=2.8)
+abline(h=0,lty=3)
+
+plot(slope~mean.size,reg.results,bty='n',pch=bubble.pch,col=alpha(bubblecol,0.5),cex=bubblesize,bty='l',log='x',yaxt='n',xaxt='n',ann=F)
+axis(1,cex.axis=1,lwd=0,lwd.ticks=1)
+axis(2,cex.axis=1,lwd=0,lwd.ticks=1)
+#title(ylab=slope~(beta),line=2.8)
+title(xlab='max length (mm)',line=2.8)
+abline(h=0,lty=3)
+
+dev.off()
