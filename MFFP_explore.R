a85e9ccac359416d5c54f1db1617198e98f0a965
V_Fugere <vincent.fugere@mail.mcgill.ca>
2020-04-06 18:00:05 -0400

    added a lot of GIS stuff

--- MFFP_explore.R
+++ MFFP_explore.R
@@ -18,14 +18,14 @@ good.surveys <- c('PENDJ','PENOF','PENT','PENOC')
 
 LCE <- read_xlsx('/Users/vincentfugere/Google Drive/Recherche/Lake Pulse Postdoc/data/GIS output/tous_les_LCE.xlsx')
 
-#en date du 7 feb 2020, les données de PS sont inutilisables (on nous a fourni les quotas, pas les captures...)
-PS <- read_xlsx(skip=2,'/Users/vincentfugere/Google Drive/Recherche/Lake Pulse Postdoc/data/MFFP/IFA - Rapport Pêche Sportive 2000-2018.xlsx')
-colnames(PS)[2] <- 'LCE'
-#n_distinct(PS$LCE) #9135 sites!
-#sum(is.na(PS$LCE))
-PS %>% distinct(LCE) %>% select(LCE) -> ps_sites
-ps_sites <- left_join(ps_sites, LCE)
-#table(ps_sites$ecosysteme) #8918 lacs, 53 rivières
+# #en date du 7 feb 2020, les données de PS sont inutilisables (on nous a fourni les quotas, pas les captures...)
+# PS <- read_xlsx(skip=2,'/Users/vincentfugere/Google Drive/Recherche/Lake Pulse Postdoc/data/MFFP/IFA - Rapport Pêche Sportive 2000-2018.xlsx')
+# colnames(PS)[2] <- 'LCE'
+# #n_distinct(PS$LCE) #9135 sites!
+# #sum(is.na(PS$LCE))
+# PS %>% distinct(LCE) %>% select(LCE) -> ps_sites
+# ps_sites <- left_join(ps_sites, LCE)
+# #table(ps_sites$ecosysteme) #8918 lacs, 53 rivières
 
 INV <- read_xlsx(skip=5,"/Users/vincentfugere/Google Drive/Recherche/Lake Pulse Postdoc/data/MFFP/IFD- 1 - Rapport Inventaire sur plan d'eau (IPE) 2000-2018.xlsx")
 colnames(INV)[1] <- 'LCE'
@@ -38,28 +38,59 @@ INV <- filter(INV, !is.na(abund))
 
 INV %>% distinct(LCE, .keep_all = T) %>% select(LCE,`Nom plan d'eau`,`Type de pêche`) -> inv_sites
 inv_sites <- left_join(inv_sites, LCE)
-sum(inv_sites$LCE %in% LCE$LCE)/nrow(inv_sites) # 98.6 % of sites have LCE
-table(inv_sites$ecosysteme) #2357 lacs, 864 rivières
-table(inv_sites$`Type de pêche`)
 
 #extracting PEN and PNN from DB
 PNN <- filter(INV, `Type de pêche` == 'PNN')
 PEN <- filter(INV, `Type de pêche` %in% good.surveys)
 PEN <- filter(PEN, `Engin` == 'Filet expérimental') #quelques trucs qui devraient pas être là 
+PEN <- filter(PEN, LCE != '04060000') #removing the one river site, which might be in Reservoir des Sables (LCE suspicious)
 
-PEN %>% distinct(LCE, .keep_all = T) %>% select(LCE,`Nom plan d'eau`,`Type de pêche`) -> PEN_sites
+# #can i use LCE to identify sites?
+# sum(is.na(PEN$LCE))
+# sum(str_length(PEN$LCE) > 1)
+# #yes, all sites have an LCE
+
+#making a clean site list that includes LCE & RHS identifiers
+PEN %>% arrange(LCE, desc(yr)) %>% distinct(LCE, .keep_all = T) -> PEN_sites
+colnames(PEN_sites) <- c('LCE','mffp_PEN_nom','survey','gear','station','valid','hasard','mffp_lat','mffp_long','date','species','captures','nb.weighed','total.mass.g','min.length.mm','max.length.mm','yr','abund')
+PEN_sites <- select(PEN_sites, LCE:survey, mffp_lat:date, yr)
 PEN_sites <- left_join(PEN_sites, LCE)
-# noms <- PEN_sites[,c(2,8)]
-# writexl::write_xlsx(noms,'~/Desktop/noms.xlsx')
+PEN_sites$mffp_coords <- paste(PEN_sites$mffp_lat,',',PEN_sites$mffp_long)
+PEN_sites$LCE_coords <- paste(PEN_sites$lce_lat,',',PEN_sites$lce_long)
+PEN_sites <- PEN_sites %>% rename(distance_lce_rhs_m = distance)
+#calculating distance between LCE centroid & MFFP sampling coordinate to help identify problematic sites
+dists <- numeric(0)
+for(i in 1:nrow(PEN_sites)){
+  tmp <- PEN_sites[i,]
+  if(is.na(tmp$mffp_lat)){dist <- NA}else if(is.na(tmp$lce_lat)){dist <- NA}else{
+    dist <- spDists(x=as.matrix(tmp[,c('mffp_long','mffp_lat')]), y =as.matrix(tmp[,c('lce_long','lce_lat')]), longlat = T)}
+  dists <- c(dists,as.numeric(dist))
+}
+PEN_sites$distance_lce_mffp_km <- dists
+PEN_sites <- PEN_sites %>% select(LCE,mffp_PEN_nom,lce_nom,rhs_nom,distance_lce_rhs_m,distance_lce_mffp_km,mffp_lat,lce_lat,mffp_long,lce_long,mffp_coords,LCE_coords,survey,date,yr,ecosysteme,RHS,lce_masl,lce_perim_km,lce_area_sq.km,rhs_area_ha,rhs_perim_m)
+#writexl::write_xlsx(PEN_sites,'~/Desktop/MFFP_PEN_sites.xlsx')
+
+#a lot of quality control done in QGIS & Excel
+#1) confirm that all lakes have the same name in 3 DB (MFFP, LCE, RHS)
+#2) when they don't, confirm that mffp % lce fall in the same polygon, and that polygon from the RHS is the right one
+#3) Some LCEs were badly entered by the MFFP. Found the right ones using coords and GIS.
+#   -TPO040600 -> F3280
+#   -O040600 -> F3280
+#   -TPJulesL -> F4944
+#   -TPpluto -> F4945
+#   -F0627 -> F3276
+#4) Some LCEs are missing from the GRHQ, so spatial join returned an erroneous polygon from the RHS.
+#   RHS for these was deleted. These were: 02226 - 
+#5) some LCEs are just bad. No coords, or coords outside of Qc, and the LCE is erreoneous.
+#   Nothing we can do with that so delete the data before analyses. These are: J2288 - J2290 - TP00001
+
+
 PNN %>% distinct(LCE, .keep_all = T) %>% select(LCE,`Nom plan d'eau`,`Type de pêche`) -> PNN_sites
 PNN_sites <- left_join(PNN_sites, LCE)
 sum(PNN_sites$LCE %in% PEN_sites$LCE)
 
-PEN_sites$LCE[PEN_sites$ecosysteme == 'riviere']
-PEN <- filter(PEN, LCE != '04060000') #removing the one river
-
-#exporter fichiers
-PEN %>% group_by(`Espèce`) %>% summarize(abund = sum(`abund`)) %>% arrange(desc(abund)) %>% writexl::write_xlsx(., '~/Desktop/poissonsMFFP.xlsx')
+# #exporter fichiers
+# PEN %>% group_by(`Espèce`) %>% summarize(abund = sum(`abund`)) %>% arrange(desc(abund)) %>% writexl::write_xlsx(., '~/Desktop/poissonsMFFP.xlsx')
 
 # ## recoupement avec autres BDD du Qc
 # load("/Users/vincentfugere/Google Drive/Recherche/Lake Pulse Postdoc/data/GIS output/QGIS_output_fishab.qgz.RData")
--- tous_les_LCE.R
+++ tous_les_LCE.R
@@ -6,19 +6,19 @@
 
 library(tidyverse)
 library(readxl)
+library(sp)
+library(rgdal)
 
-load("/Users/vincentfugere/Google Drive/Recherche/Lake Pulse Postdoc/data/GIS/QGIS_output_fishab.qgz.RData")
+load("/Users/vincentfugere/Google Drive/Recherche/Lake Pulse Postdoc/data/GIS output/QGIS_output_fishab.qgz.RData")
 
-LCE_lac <- select(LCE_lac, NO_LAC, ID_RHS, LATITUDE_DEGRE_DEC_NAD83, LONGITUDE_DEGRE_DEC_NAD83, NOM_LAC, NOM_LAC_MINUSCULE)
-colnames(LCE_lac)[1] <- 'LCE'
-colnames(LCE_lac)[2:6] <- c('RHS','lat','long','nom','nom.minuscule')
+LCE_lac <- select(LCE_lac, NO_LAC, ID_RHS, NOM_LAC_MINUSCULE, NOM_LAC, LATITUDE_DEGRE_DEC_NAD83, LONGITUDE_DEGRE_DEC_NAD83, ALTITUDE_LAC, PERIMETRE_LAC, SUPERFICIE_NETTE_LAC, SUP_HA, PERIM_M, TOPONYME, Distance)
+colnames(LCE_lac) <- c('LCE', 'RHS','lce_nom.minuscule','lce_nom','lce_lat','lce_long','lce_masl','lce_perim_km','lce_area_sq.km','rhs_area_ha','rhs_perim_m','rhs_nom','distance')
 LCE_lac$ecosysteme <- 'lac'
 
-LCE_riv <- select(LCE_riv, NO_COURS_DEAU, ID_RHS, LATITUDE_DEG_DEC_NAD83, LONGITUDE_DEG_DEC_NAD83, NOM_COURS_DEAU, NOM_COURS_DEAU_MINUSCULE)
-colnames(LCE_riv)[1] <- 'LCE'
-colnames(LCE_riv)[2:6] <- c('RHS','lat','long','nom','nom.minuscule')
+LCE_riv <- select(LCE_riv, NO_COURS_DEAU, ID_RHS, NOM_COURS_DEAU_MINUSCULE, NOM_COURS_DEAU, LATITUDE_DEG_DEC_NAD83, LONGITUDE_DEG_DEC_NAD83, SUP_HA, PERIM_M, TOPONYME, Distance)
+colnames(LCE_riv) <- c('LCE','RHS','lce_nom.minuscule','lce_nom','lce_lat','lce_long','rhs_area_ha','rhs_perim_m','rhs_nom','distance')
 LCE_riv$ecosysteme <- 'riviere'
 
-touslesLCE <- bind_rows(LCE_lac,LCE_riv)
-writexl::write_xlsx(touslesLCE, '~/Desktop/tous_les_LCE.xlsx')
+touslesLCE <- bind_rows(LCE_lac,LCE_riv) %>% select(ecosysteme, LCE, RHS, everything())
+writexl::write_xlsx(touslesLCE, '/Users/vincentfugere/Google Drive/Recherche/Lake Pulse Postdoc/data/GIS output/tous_les_LCE.xlsx')
 
